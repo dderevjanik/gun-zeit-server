d6b54985b0208334b550799e98d1a133f8ede2ac
dderevjanik <daniel.derevjanik@gmail.com>
2019-12-01 15:42:39 +0100

    Unubo deployment

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const Gun = require('gun');
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(Gun.serve);
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + "/public"));
 
-const server = app.listen(3000);
+const server = app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
 const gun = Gun({ file: 'data', web: server });
-console.log(gun);
+
+
